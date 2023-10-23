afdb455081524f6e9cc80cf02c6cfafdb701b4cb
Sam <samuel.fitz@btinternet.com>
2023-10-23 20:58:09 +0100

    feat: change form to take in rugby users

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,40 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 
 const Form = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [formData, setFormData] = useState({
+    user: "",
+    game1: "",
+    game1FirstTry: "",
+    game2: "",
+    game2FirstTry: "",
+    game3: "",
+    game3FirstTry: "",
+  });
 
-  function handleFirstName(event: any) {
-    setFirstName(event.target.value);
+  function handleChange(event: any) {
+    const {name, value} = event.target
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value
+
+      }});
   }
 
-  function handleLastName(event: any) {
-    setLastName(event.target.value);
+  function handleSubmit(event:any ) {
+    event.preventDefault()
+    console.log(formData);
   }
 
   return (
     <>
-      <form>
-        <input type="text" placeholder="First Name" onChange={handleFirstName} />
-        <input type="text" placeholder="Last Name" onChange={handleLastName} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="First Name" onChange={handleChange} name="user" value={formData.user}/>
+        <button>Try it</button>
       </form>
-      <h1>{firstName} {lastName}</h1>
+      <h1></h1>
     </>
   );
 };
